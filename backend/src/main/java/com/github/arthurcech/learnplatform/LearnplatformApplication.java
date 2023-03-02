0bc37faaf8ca7da61b3fc883bf6b2b1ceb2609a0
Arthur Cech Francisco <arthurcech@gmail.com>
2023-03-02 13:02:11 -0300

    create backend

---
+++ backend/src/main/java/com/github/arthurcech/learnplatform/LearnplatformApplication.java
@@ -0,0 +1,13 @@
+package com.github.arthurcech.learnplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnplatformApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/arthurcech/learnplatform/LearnplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.arthurcech.learnplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
